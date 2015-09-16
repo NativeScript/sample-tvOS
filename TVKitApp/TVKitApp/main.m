cd3f2aa5f2ee2267234545dea6dcaeb3c3e530dc
Jason Zhekov <jasssonpet@gmail.com>
2015-09-16 15:48:34 +0300

    Add empty AppleTV project

---
+++ TVKitApp/TVKitApp/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  TVKitApp
+//
+//  Created by Jason Zhekov on 9/16/15.
+//  Copyright © 2015 Telerik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, nil);
+    }
+}
